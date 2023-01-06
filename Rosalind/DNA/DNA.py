25b18e828ec010eafc12ab7fd7ffe8435d597bdd
Joshua Mitchell <joshuamerrittmitchell@gmail.com>
2023-01-06 12:20:01 -0700

    made output order of DNA generic

--- Rosalind/DNA/DNA.py
+++ Rosalind/DNA/DNA.py
@@ -1,10 +1,10 @@
 from collections import Counter, defaultdict
 
-def solution_DNA(dataset_path):
+def solution_DNA(dataset_path, output_order=["A", "C", "G", "T"]):
     with open(dataset_path) as dataset_filehandle:
         nucleotide_string = dataset_filehandle.readlines()[0].rstrip()
     nucleotide_counter = defaultdict(int, Counter(nucleotide_string))
-    return nucleotide_counter["A"], nucleotide_counter["C"], nucleotide_counter["G"], nucleotide_counter["T"]
+    return [nucleotide_counter[x] for x in output_order]
 
 if __name__ == '__main__':
     import sys
